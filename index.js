be509c55a8bd44a44880091c4125cc4a078f18d9
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-20 14:07:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your solution in this file!
 const driver = {};
 
+<<<<<<< HEAD
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
@@ -25,3 +26,13 @@ function destructivelyDeleteFromDriverByKey(driver, key) {
 
   return driver;
 }
+=======
+updateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign({}, driver, { [key]: value });
+}
+
+destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver;
+}  
+>>>>>>> 7dbd304f37ea7e2d4d5a36ccd3b3dd79c26f94c7
