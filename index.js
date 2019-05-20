7dbd304f37ea7e2d4d5a36ccd3b3dd79c26f94c7
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-20 13:49:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const driver = {};
+
+updateDriverWithKeyAndValue(driver, key, value){
+  return Object.assign({}, driver, { [key]: value });
+}
+
+destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  driver[key] = value;
+  return driver;
+}  
